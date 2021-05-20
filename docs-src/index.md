0190ee06f5db2b3d8a073b4d2441dfcbed0d1b21
Eli Bishop <eli@launchdarkly.com>
2021-05-20 10:30:03 -0700

    initial implementation for M.E.L 3.x

---
+++ docs-src/index.md
@@ -0,0 +1,38 @@
+This .NET package provides integration from the [`LaunchDarkly.Logging`](https://launchdarkly.github.io/dotnet-logging) API that is used by the LaunchDarkly [.NET SDK](https://github.com/launchdarkly/dotnet-server-sdk), [Xamarin SDK](https://github.com/launchdarkly/xamarin-client-sdk), and other LaunchDarkly libraries, to the Microsoft logging framework [`Microsoft.Extensions.Logging`](https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.logging?view=dotnet-plat-ext-5.0).
+
+This adapter is published as a separate NuGet package to avoid unwanted dependencies on `Microsoft.Extensions.Logging` in the LaunchDarkly SDKs and in applications that do not use that framework.
+
+The [`LaunchDarkly.Logging`](https://launchdarkly.github.io/dotnet-logging/) also contains a version of this adapter, `Logs.CoreLogging`, which was implemented only in the .NET Core target framework and always uses version 5.x of `Microsoft.Extensions.Logging`. `LaunchDarkly.Logging.Microsoft` is the more broadly compatible version of that.
+
+## Usage
+
+The **<xref:LaunchDarkly.Logging.LdMicrosoftLogging>** adapter is provided by the NuGet package [**`LaunchDarkly.Logging.Microsoft`**](https://nuget.org/packages/LaunchDarkly.Logging.Microsoft). Version 1.x of the package works with `Microsoft.Extensions.Logging` version 3.x. Higher major versions of the package can be used with later versions of `Microsoft.Extensions.Logging`.
+
+Like `LaunchDarkly.Logging`, `Microsoft.Extensions.Logging` is a facade that can delegate to various destinations. These are configured with the [`LoggerFactory`](https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.logging.loggerfactory?view=dotnet-plat-ext-5.0) or by dependency injection mechanisms that provide an `ILoggerFactory`. 
+
+`LaunchDarkly.Logging` already has adapters of its own for some of the same basic destinations that `Microsoft.Extensions.Logging` can write to. For instance, to send log output from LaunchDarkly components to the console, or to a file, you do not need to use `Microsoft.Extensions.Logging`; you can simply use the methods in `LaunchDarkly.Logging.Logs`. This adapter is mainly useful if you require the more advanced configuration that `Microsoft.Extensions.Logging` provides, or if you have an application that is already using `Microsoft.Extensions.Logging`.
+
+To use the adapter:
+
+1. Add the NuGet package `LaunchDarkly.Logging.Microsoft` to your project. Make sure you also have a dependency on a compatible version of [`Microsoft.Extensions.Logging`](https://nuget.org/packages/Microsoft.Extensions.Logging).
+
+2. Use the property [**LdMicrosoftLogging.Adapter**](xref:LaunchDarkly.Logging.LdMicrosoftLogging.Adapter) in any LaunchDarkly library configuration that accepts a `LaunchDarkly.Logging.ILogAdapter` object. It requires an `ILoggerFactory`, which in .NET Core is often obtained by dependency injection. For instance, if you are configuring the LaunchDarkly .NET SDK in a component that uses dependency injection:
+
+```csharp
+using LaunchDarkly.Logging;
+using LaunchDarkly.Sdk.Server;
+using Microsoft.Extensions.Logging;
+
+public class MyApplicationComponent
+{
+    private LdClient ldClient;
+
+    public MyApplicationComponent(ILoggerFactory loggerFactory)
+    {
+        var config = Configuration.Builder("my-sdk-key")
+            .Logging(LdMicrosoftLogging.Adapter(loggerFactory))
+            .Build();
+        ldClient = new LdClient(config);
+    }
+}
+```
---
+++ src/LaunchDarkly.Logging.Microsoft/LdMicrosoftLogging.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LaunchDarkly.Logging
+{
+    /// <summary>
+    /// Provides integration between the LaunchDarkly SDK's logging framework and
+    /// the <c>Microsoft.Extensions.Logging</c> API.
+    /// </summary>
+    public static class LdMicrosoftLogging
+    {
+        /// <summary>
+        /// A logging implementation that delegates to the <c>Microsoft.Extensions.Logging</c> framework.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This method is only available when your target framework is .NET Core. It causes
+        /// the <c>LaunchDarkly.Logging</c> APIs to delegate to the <c>Microsoft.Extensions.Logging</c>
+        /// framework. The <c>ILoggerFactory</c> is the main configuration object for
+        /// <c>Microsoft.Extensions.Logging</c>; application code can construct it programmatically,
+        /// or can obtain it by dependency injection. For more information, see
+        /// <see href="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1">Logging
+        /// in .NET Core and ASP.NET Core</see>.
+        /// </para>
+        /// <para>
+        /// The <c>Microsoft.Extensions.Logging</c> framework has its own mechanisms for filtering log
+        /// output by level or other criteria. If you add a level filter with
+        /// <see cref="ILogAdapterExtensions.Level(ILogAdapter, LogLevel)"/>, it will filter
+        /// out messages below that level before they reach the Microsoft logger.
+        /// </para>
+        /// </remarks>
+        /// <param name="loggerFactory">the factory object for Microsoft logging</param>
+        /// <returns>a logging adapter</returns>
+        public static ILogAdapter Adapter(ILoggerFactory loggerFactory) =>
+            new MicrosoftLoggingAdapter(loggerFactory);
+    }
+
+    internal sealed class MicrosoftLoggingAdapter : ILogAdapter
+    {
+        private readonly ILoggerFactory _factory;
+
+        internal MicrosoftLoggingAdapter(ILoggerFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public IChannel NewChannel(string name) =>
+            new ChannelImpl(_factory.CreateLogger(name));
+   
+        private class ChannelImpl : IChannel
+        {
+            private readonly ILogger _logger;
+
+            internal ChannelImpl(ILogger logger)
+            {
+                _logger = logger;
+            }
+
+            private static Microsoft.Extensions.Logging.LogLevel Level(LogLevel level)
+            {
+                switch (level)
+                {
+                    case LogLevel.Debug:
+                        return Microsoft.Extensions.Logging.LogLevel.Debug;
+                    case LogLevel.Info:
+                        return Microsoft.Extensions.Logging.LogLevel.Information;
+                    case LogLevel.Warn:
+                        return Microsoft.Extensions.Logging.LogLevel.Warning;
+                    case LogLevel.Error:
+                        return Microsoft.Extensions.Logging.LogLevel.Error;
+                    default:
+                        return Microsoft.Extensions.Logging.LogLevel.None;
+                }
+            }
+
+            public bool IsEnabled(LogLevel level) => _logger.IsEnabled(Level(level));
+
+            public void Log(LogLevel level, object message)
+            {
+                var msLevel = Level(level);
+                if (_logger.IsEnabled(msLevel))
+                {
+                    _logger.Log(msLevel, message.ToString(), null);
+                }
+            }
+
+            public void Log(LogLevel level, string format, object param)
+            {
+                var msLevel = Level(level);
+                if (_logger.IsEnabled(msLevel))
+                {
+                    _logger.Log(msLevel, format, param);
+                }
+            }
+
+            public void Log(LogLevel level, string format, object param1, object param2)
+            {
+                var msLevel = Level(level);
+                if (_logger.IsEnabled(msLevel))
+                {
+                    _logger.Log(msLevel, format, param1, param2);
+                }
+            }
+
+            public void Log(LogLevel level, string format, params object[] allParams)
+            {
+                var msLevel = Level(level);
+                if (_logger.IsEnabled(msLevel))
+                {
+                    _logger.Log(msLevel, format, allParams);
+                }
+            }
+        }
+    }
+}
